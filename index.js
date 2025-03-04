ee3f6e614a7a6ea28b75ccc62b187167d9399c69
Jeremy M <jman2476@live.com>
2025-03-04 14:54:50 -0500

    wrote non IIFE version of 2x2 animation function, added export statement

--- index.js
+++ index.js
@@ -54,8 +54,8 @@ function animate(arr) {
 
 // setTimeout(clearInterval, 2000, twoBy2)
 
-// TODO: write non IIFE version of 1 line animation function
-function waitingAnimation(animArr) {
+// 1 line animation function
+function loadAnimation(animArr) {
     let index = 0;
     const cycler = animArr.length - 1
     const timerId = setInterval(() => {
@@ -64,6 +64,27 @@ function waitingAnimation(animArr) {
     }, 250)
     return timerId
 }
+// Example: Run loading animation for 3 seconds
+//      const animID = loadAnimation(dots)
+//      setTimeout(clearInterval, 3000, animID)
 
-const animID = waitingAnimation(dots)
-setTimeout(clearInterval, 3000, animID)
+// TODO: write non IIFE version of 2x2 animation function
+function loadSquareAnim2(animArr) {
+    let index = 0
+    const cycler = animArr.length - 1
+    stdout.write('\n')
+    const timerId = setInterval(() => {
+        stdout.moveCursor(-2,-1)
+        stdout.write(animArr[index++])
+        index &= cycler
+    }, 250)
+    return timerId
+}
+// Example: Run loading animation for 3 seconds
+//      const animSqr = loadSquareAnim2(spin_waves)
+//      setTimeout(clearInterval, 3000, animSqr)
+
+export {
+    loadAnimation,
+    loadSquareAnim2
+}
